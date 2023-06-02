edabde07da8009f18667a27463e53773a50dd1af
kabul225 <kabul250200@gmail.com>
2023-06-02 17:29:07 +0500

    checking for changes on models.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -10,7 +10,7 @@ class CalcTest(SimpleTestCase):
     def test_add(self):
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_subtract(self):
         res = calc.subtract(10, 5)
         self.assertEqual(res, 5)
