f431fe6875f7cd76b70e1fd3953c3be976ed7ffc
kabul225 <kabul250200@gmail.com>
2023-06-02 17:22:18 +0500

    checking for changes on admin.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -9,10 +9,8 @@ from app import calc
 class CalcTest(SimpleTestCase):
     def test_add(self):
         res = calc.add(5, 6)
-        
         self.assertEqual(res, 11)
     
     def test_subtract(self):
         res = calc.subtract(10, 5)
-
         self.assertEqual(res, 5)
